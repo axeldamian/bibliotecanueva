cd9aef301385ad0f869a518b6d5e77d54f251285
axeldamian <axel.arrondo@mercadolibre.com>
2024-12-10 21:54:51 -0300

    javadoc comments added

--- newlibrary/src/main/java/com/newlibrary/App.java
+++ newlibrary/src/main/java/com/newlibrary/App.java
@@ -1,11 +1,18 @@
 package com.newlibrary;
 
-/**
- * Hello world!
- *
- */
-public class App 
+    /**
+     * Main class of the application.
+     * @author Axel Arrondo
+     * @version 1.0
+     */
+public class App
 {
+
+    /**
+     * Main input function of the project.
+     * @author Axel Arrondo
+     * @version 1.0
+     */
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
--- newlibrary/src/main/java/com/newlibrary/clases/Operations.java
+++ newlibrary/src/main/java/com/newlibrary/clases/Operations.java
@@ -1,7 +1,30 @@
 package com.newlibrary.clases;
 
+    /**
+     * Class of operations.
+     * @author Axel Arrondo
+     * @version 1.0
+     */
 public class Operations {
 
+    /**
+     * Empty constructor.
+     * @author Axel Arrondo
+     * @version 1.0
+     */
+    public Operations(){
+        // empty
+    }
+
+    /**
+     * Add two numbers.
+     *
+     * @author Axel Arrondo
+     * @version 1.0
+     * @param first param is a int.
+     * @param second param is a int.
+     * @return Sum of number, is a int.
+     */
     public int sum(int a, int b){
         return a + b;
     }
