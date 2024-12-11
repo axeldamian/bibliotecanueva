35a3f22cdb4b69dd3aab524186ac9fd9d862db72
axeldamian <axel.arrondo@mercadolibre.com>
2024-12-11 15:58:17 -0300

    gpg sign, javadoc and jar

---
+++ newlibrary/target/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.newlibrary","l":"App"},{"p":"com.newlibrary.clases","l":"Operations"}];updateSearchResults();
