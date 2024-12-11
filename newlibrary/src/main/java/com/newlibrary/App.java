d4809e640e38d909343e0923f5ed622f06f212c5
axeldamian <axel.arrondo@mercadolibre.com>
2024-12-10 22:00:52 -0300

    javadoc generated

--- newlibrary/src/main/java/com/newlibrary/App.java
+++ newlibrary/src/main/java/com/newlibrary/App.java
@@ -10,8 +10,6 @@ public class App
 
     /**
      * Main input function of the project.
-     * @author Axel Arrondo
-     * @version 1.0
      */
     public static void main( String[] args )
     {
--- newlibrary/src/main/java/com/newlibrary/clases/Operations.java
+++ newlibrary/src/main/java/com/newlibrary/clases/Operations.java
@@ -9,8 +9,6 @@ public class Operations {
 
     /**
      * Empty constructor.
-     * @author Axel Arrondo
-     * @version 1.0
      */
     public Operations(){
         // empty
@@ -18,12 +16,6 @@ public class Operations {
 
     /**
      * Add two numbers.
-     *
-     * @author Axel Arrondo
-     * @version 1.0
-     * @param first param is a int.
-     * @param second param is a int.
-     * @return Sum of number, is a int.
      */
     public int sum(int a, int b){
         return a + b;
