2eae5d7822ac44ecc1e1dc91d0306e73d557c26f
axeldamian <axel.arrondo@mercadolibre.com>
2024-12-16 17:43:09 -0300

    modifique el namespace

--- newlibrary/target/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.newlibrary","l":"App"},{"p":"com.newlibrary.classes","l":"Operations"}];updateSearchResults();
---
+++ newlibraryleibnix/target/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.newlibraryleibnix"},{"l":"com.newlibraryleibnix.classes"}];updateSearchResults();
