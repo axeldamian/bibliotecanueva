21de0b928664287e241cc47fab6f49c8ba1b0ab6
axeldamian <axel.arrondo@mercadolibre.com>
2024-12-17 18:59:13 -0300

    ejecute  mvn clean compile javadoc:jar javadoc:javadoc package verify

---
+++ newlibrary/target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.newlibrary","l":"App"},{"p":"com.newlibrary.classes","l":"Operations"}];updateSearchResults();
