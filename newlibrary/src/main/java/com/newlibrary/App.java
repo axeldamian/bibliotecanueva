0fb0f60e29d2b1ad0c23f9ccf1c24c0d5937fe75
axeldamian <axel.arrondo@mercadolibre.com>
2024-12-17 18:54:56 -0300

    renombre cosas

---
+++ newlibrary/src/main/java/com/newlibrary/App.java
@@ -0,0 +1,19 @@
+package com.newlibrary;
+
+    /**
+     * Main class of the application.
+     * @author Axel Arrondo
+     * @version 1.0
+     */
+public class App
+{
+
+    /**
+     * Main input function of the project.
+     */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+}
